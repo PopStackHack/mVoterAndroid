2c6917fca28f2d866336baddc7075625b669bfd2
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-07-13 10:31:17 +0630

    fix: Upgrade livedata to fix issue on lint check
    See: https://developer.android.com/jetpack/androidx/releases/lifecycle#2.3.0-alpha04

--- buildSrc/src/main/kotlin/AndroidXArch.kt
+++ buildSrc/src/main/kotlin/AndroidXArch.kt
@@ -10,12 +10,12 @@ fun DependencyHandler.androidXArch() {
 }
 
 object AndroidXArchLifeCycle {
-  private const val version = "2.2.0"
+  private const val version = "2.3.0-alpha05"
 
   const val view_model = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
   const val saved_state = "androidx.lifecycle:lifecycle-viewmodel-savedstate:$version"
   const val live_data = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
-  const val extension = "androidx.lifecycle:lifecycle-extensions:$version"
+  const val extension = "androidx.lifecycle:lifecycle-extensions:2.2.0"
   const val lifecycle_java8 = "androidx.lifecycle:lifecycle-common-java8:$version"
   const val lifecycle_compiler = "androidx.lifecycle:lifecycle-compiler:$version"
   const val lifecycle_service = "androidx.lifecycle:lifecycle-service:$version"
