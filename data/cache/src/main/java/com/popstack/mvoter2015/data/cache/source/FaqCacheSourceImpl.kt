89c7b7d95068819df49934343760b85cb623a1e1
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-07-21 11:41:29 +0630

    feat: Add Ballot Example Table

--- data/cache/src/main/java/com/popstack/mvoter2015/data/cache/source/FaqCacheSourceImpl.kt
+++ data/cache/src/main/java/com/popstack/mvoter2015/data/cache/source/FaqCacheSourceImpl.kt
@@ -3,6 +3,7 @@ package com.popstack.mvoter2015.data.cache.source
 import com.popstack.mvoter2015.data.cache.MVoterDb
 import com.popstack.mvoter2015.data.cache.entity.FaqTable
 import com.popstack.mvoter2015.data.common.faq.FaqCacheSource
+import com.popstack.mvoter2015.domain.faq.model.BallotExample
 import com.popstack.mvoter2015.domain.faq.model.Faq
 import com.popstack.mvoter2015.domain.faq.model.FaqCategory
 import javax.inject.Inject
@@ -35,13 +36,38 @@ class FaqCacheSourceImpl @Inject constructor(
       limit = limit.toLong(),
       offset = offset.toLong()
     ).executeAsList().map {
-      it.mapToEntity(db)
+      it.mapToEntity()
+    }
+  }
+
+  override fun putBallotExampleList(ballotExampleList: List<BallotExample>) {
+    db.transaction {
+      ballotExampleList.forEach { ballotExample ->
+        db.ballotExampleTableQueries.insertOrReplace(
+          id = ballotExample.id,
+          image = ballotExample.image,
+          isValid = ballotExample.isValid,
+          reason = ballotExample.reason
+        )
+      }
+    }
+  }
+
+
+  override fun getBallotExampleList(): List<BallotExample> {
+    return db.ballotExampleTableQueries.selectAll().executeAsList().map { table ->
+      BallotExample(
+        id = table.id,
+        image = table.image,
+        isValid = table.isValid,
+        reason = table.reason
+      )
     }
   }
 
 }
 
-fun FaqTable.mapToEntity(db: MVoterDb): Faq {
+fun FaqTable.mapToEntity(): Faq {
   return Faq(
     faqId = id,
     question = question,
