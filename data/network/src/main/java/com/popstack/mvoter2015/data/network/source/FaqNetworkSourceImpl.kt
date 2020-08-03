b515fc603f68fc6a5700cf620069abb177a5a061
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-08-03 21:24:41 +0630

    fix: Remove the dummy text for faq data

--- data/network/src/main/java/com/popstack/mvoter2015/data/network/source/FaqNetworkSourceImpl.kt
+++ data/network/src/main/java/com/popstack/mvoter2015/data/network/source/FaqNetworkSourceImpl.kt
@@ -5,7 +5,6 @@ import com.popstack.mvoter2015.data.network.api.MvoterApi
 import com.popstack.mvoter2015.domain.faq.model.BallotExample
 import com.popstack.mvoter2015.domain.faq.model.Faq
 import com.popstack.mvoter2015.domain.faq.model.FaqCategory
-import com.popstack.mvoter2015.domain.faq.model.FaqId
 import javax.inject.Inject
 
 class FaqNetworkSourceImpl @Inject constructor(
@@ -13,19 +12,7 @@ class FaqNetworkSourceImpl @Inject constructor(
 ) : FaqNetworkSource {
 
   override fun getFaqList(page: Int, itemsPerPage: Int, category: FaqCategory): List<Faq> {
-    Thread.sleep(500)
-    return (1..10).map {
-      Faq(
-        faqId = FaqId(it.toString()),
-        question = "Question ($it)",
-        answer = "အဖြေကတော့ မရှိသေးပါဘူးခင်ဗျာ။ သိချင်ရင်တော့ ဆုတောင်းပါဗျာ။ ဆုတောင်းရင် အကုန်ဖြစ်တယ်။ မဖြစ်တာဆိုလို့ မင်းပဲရှိတယ်။ စဉ်းစားပေ့ါကွာ",
-        lawSource = "",
-        articleSource = "",
-        category = FaqCategory.GENERAL,
-        shareableUrl = ""
-      )
-    }
-//    throw NetworkException(errorBody = "", errorCode = 404)
+    TODO("Not yet implemented")
   }
 
   override fun getBallotExampleList(): List<BallotExample> {
