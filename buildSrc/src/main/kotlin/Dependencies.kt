7ca2f2c99a9d0ee5b160a1c51aa662100a5e35e5
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-08-24 13:20:15 +0630

    config: Upgrade to Kotlin 1.4 in third party libraries

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -18,33 +18,15 @@ object BuildConfig {
 object CommonLibs {
   const val android_gradle_plugin = "com.android.tools.build:gradle:4.0.1"
 
-  const val phrase = "com.squareup.phrase:phrase:1.1.0"
-  const val sonar = "com.facebook.sonar:sonar:0.0.1"
   const val timber = "com.jakewharton.timber:timber:4.7.1"
-  const val junit = "junit:junit:4.12"
+  const val junit = "junit:junit:4.13"
   const val javaxInject = "javax.inject:javax.inject:1"
   const val javaXAnnotations = "org.glassfish:javax.annotation:10.0-b28"
-
-  const val easy_image = "com.github.jkwiecien:EasyImage:3.0.3"
-  const val android_crop = "com.soundcloud.android:android-crop:1.0.1@aar"
-  const val ucrop = "com.github.yalantis:ucrop:2.2.2"
-  const val fastscroll = "com.simplecityapps:recyclerview-fastscroll:2.0.0"
-
-  const val colorpicker = "com.github.QuadFlask:colorpicker:0.0.13"
-  const val circle_image_view = "de.hdodenhof:circleimageview:3.0.1"
-  const val shape_of_views = "com.github.florent37:shapeofview:1.4.7"
-
-  const val burtha = "com.aungkyawpaing.burtha:burtha-kt:1.1.1"
-
 }
 
 //region AndroidX
-object AndroidXAnnotations {
-  const val annotations = "androidx.annotation:annotation:1.1.0"
-}
-
 object AndroidXAppCompat {
-  const val app_compat = "androidx.appcompat:appcompat:1.3.0-alpha01"
+  const val app_compat = "androidx.appcompat:appcompat:1.3.0-alpha02"
 }
 
 object AndroidXRecyclerView {
@@ -59,7 +41,7 @@ object AndroidXCardView {
 }
 
 object AndroidXConstraintLayout {
-  private const val version = "2.0.0-rc1"
+  private const val version = "2.0.0"
 
   const val constraint_layout = "androidx.constraintlayout:constraintlayout:$version"
 }
@@ -70,31 +52,15 @@ object AndroidXViewPager {
   const val view_pager_2 = "androidx.viewpager2:viewpager2:1.1.0-alpha01"
 }
 
-object AndroidXLegacy {
-  private const val version = "1.0.0"
-
-  const val support_v4 = "androidx.legacy:legacy-support-v4:$version"
-}
-
-object AndroidXPalette {
-  const val palette = "androidx.palette:palette:1.0.0"
-}
-
 object AndroidXPreference {
-  private const val version = "1.1.0"
+  private const val version = "1.1.1"
 
   const val preference = "androidx.preference:preference-ktx:$version"
   const val preference_ktx = "androidx.preference:preference-ktx:$version"
 }
 
-object AndroidXSecurity {
-  private const val version = "1.0.0-alpha02"
-
-  const val crypto = "androidx.security:security-crypto:$version"
-}
-
 object AndroidXSqlite {
-  private const val version = "2.1.0-alpha01"
+  private const val version = "2.1.0"
 
   const val sqlite = "androidx.sqlite:sqlite:$version"
   const val sqlite_framework = "androidx.sqlite:sqlite-framework:$version"
@@ -102,7 +68,7 @@ object AndroidXSqlite {
 }
 
 object AndroidArchWork {
-  private const val version = "2.3.0-rc01"
+  private const val version = "2.4.0"
 
   const val work_runtime = "androidx.work:work-runtime:$version"
   const val work_runtime_ktx = "androidx.work:work-runtime-ktx:$version"
@@ -111,43 +77,22 @@ object AndroidArchWork {
 }
 
 object AndroidXCore {
-  private const val version = "1.5.0-alpha01"
+  private const val version = "1.5.0-alpha02"
 
   const val core = "androidx.core:core:$version"
   const val core_ktx = "androidx.core:core-ktx:$version"
 }
 
 object AndroidXPaging {
-  private const val version = "3.0.0-alpha03"
+  private const val version = "3.0.0-alpha05"
 
   const val common = "androidx.paging:paging-common:$version"
   const val runtime = "androidx.paging:paging-runtime:$version"
 }
 
-object AndroidXLocalBroadcastManager {
-
-  const val local_broadcast_manager =
-    "androidx.localbroadcastmanager:localbroadcastmanager:1.1.0-alpha01"
-
-}
-
 //endregion
-
-object AssistedInject {
-
-  private const val version = "0.5.2"
-
-  const val annotations_dagger = "com.squareup.inject:assisted-inject-annotations-dagger2:$version"
-  const val processor_dagger = "com.squareup.inject:assisted-inject-processor-dagger2:$version"
-}
-
 object Material {
   const val material = "com.google.android.material:material:1.3.0-alpha02"
-}
-
-object Caligraphy {
-  const val caligraphy = "io.github.inflationx:calligraphy3:3.1.1"
-  const val viewpump = "io.github.inflationx:viewpump:2.0.3"
 }
 
 object Coil {
@@ -163,51 +108,12 @@ object Conductor {
   const val lifecycle = "com.bluelinelabs:conductor-archlifecycle:$version"
 }
 
-object Chuck {
-  private const val version = "1.1.0"
-
-  const val debug = "com.readystatesoftware.chuck:library:$version"
-  const val no_op = "com.readystatesoftware.chuck:library-no-op:$version"
-}
-
-object Detekt {
-  private const val version = "1.7.4"
-
-  const val plugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$version"
-
-}
-
-object EverNoteJob {
-  private const val version = "1.2.6"
-
-  const val core = "com.evernote:android-job:$version"
-}
-
 object Firebase {
   const val messaging = "com.google.firebase:firebase-messaging:20.2.4"
   const val analytics = "com.google.firebase:firebase-analytics-ktx:17.5.0"
 
   const val crashlytics = "com.google.firebase:firebase-crashlytics-ktx:17.2.1"
   const val crashlytics_plugin = "com.google.firebase:firebase-crashlytics-gradle:2.2.0"
-}
-
-object Flipper {
-  private const val version = "0.25.0"
-
-  const val flipper = "com.facebook.flipper:flipper:$version"
-  const val soloader = "com.facebook.soloader:soloader:0.5.1"
-  const val network_plugin = "com.facebook.flipper:flipper-network-plugin:$version"
-  const val image_plugin = "com.facebook.flipper:flipper-fresco-plugin:$version"
-
-  const val filpper_no_op = "com.facebook.flipper:flipper-noop:$version"
-}
-
-object Glide {
-  private const val version = "4.10.0"
-
-  const val runtime = "com.github.bumptech.glide:glide:$version"
-  const val compiler = "com.github.bumptech.glide:compiler:$version"
-  const val transformations = "jp.wasabeef:glide-transformations:4.0.1"
 }
 
 object GoogleService {
@@ -234,7 +140,7 @@ object Kotlin {
 }
 
 object KotlinCoroutine {
-  private const val version = "1.3.2"
+  private const val version = "1.3.9"
 
   const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
   const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
@@ -267,7 +173,7 @@ object Monex {
 }
 
 object OkHttp {
-  private const val version = "4.5.0"
+  private const val version = "4.8.1"
 
   const val client = "com.squareup.okhttp3:okhttp:$version"
   const val logger = "com.squareup.okhttp3:logging-interceptor:$version"
@@ -275,19 +181,14 @@ object OkHttp {
 }
 
 object Retrofit {
-  private const val version = "2.8.1"
+  private const val version = "2.9.0"
 
   const val core = "com.squareup.retrofit2:retrofit:$version"
   const val moshi_converter = "com.squareup.retrofit2:converter-moshi:$version"
 }
 
-object Renderer {
-
-  const val renderer = "com.github.pedrovgs:renderers:3.4.0"
-}
-
 object SqlDelight {
-  private const val version = "1.3.0"
+  private const val version = "1.4.1"
 
   const val gradle_plugin = "com.squareup.sqldelight:gradle-plugin:$version"
   const val android_driver = "com.squareup.sqldelight:android-driver:$version"
@@ -296,29 +197,8 @@ object SqlDelight {
   const val jvm_driver = "com.squareup.sqldelight:sqlite-driver:$version"
 }
 
-object Stetho {
-  private const val version = "1.5.1"
-
-  const val core = "com.facebook.stetho:stetho:$version"
-  const val okhttp3 = "com.facebook.stetho:stetho-okhttp3:$version"
-}
-
 object Shimmer {
   const val reycler_view = "com.github.sharish:ShimmerRecyclerView:v1.3"
-}
-
-object ThreeTenBp {
-  private const val version = "1.4.0"
-
-  const val core = "org.threeten:threetenbp:$version"
-  const val no_tz_db = "org.threeten:threetenbp:$version:no-tzdb"
-  const val android = "com.jakewharton.threetenabp:threetenabp:1.2.1"
-}
-
-object MMPhoneNo {
-  private const val version = "1.0.2"
-
-  const val core = "com.aungkyawpaing.mmphonenumber:mmphonenumber:$version"
 }
 
 object Wormhole {
