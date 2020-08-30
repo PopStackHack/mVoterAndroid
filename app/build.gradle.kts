5f18e2f6330da8f1e4dbe109a041ad28974aff9f
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-08-31 01:46:47 +0630

    fix: Add popstack dark logo back which was removed by mistake

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ android {
 
     getByName("release") {
       isMinifyEnabled = true
+      isShrinkResources = true
+      isZipAlignEnabled = true
       isDebuggable = false
       signingConfig = signingConfigs.getByName("release")
       proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
