e60e88a4ae460387b56e1f93fcabea5627f95545
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-09-16 11:43:39 +0630

    fix: License not loading on API 9+
    https://stackoverflow.com/questions/54516798/webview-loaddata-not-working-on-android-9-0-api-29

--- app/src/main/java/com/popstack/mvoter2015/feature/about/LicenseController.kt
+++ app/src/main/java/com/popstack/mvoter2015/feature/about/LicenseController.kt
@@ -2,15 +2,20 @@ package com.popstack.mvoter2015.feature.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.lifecycle.lifecycleScope
 import com.popstack.mvoter2015.R
-import com.popstack.mvoter2015.core.BaseController
+import com.popstack.mvoter2015.core.LifeCycleAwareController
 import com.popstack.mvoter2015.databinding.ControllerLicenseBinding
 import com.popstack.mvoter2015.helper.conductor.requireContext
 import com.popstack.mvoter2015.helper.conductor.setSupportActionBar
 import com.popstack.mvoter2015.helper.conductor.supportActionBar
 import java.io.IOException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
-class LicenseController : BaseController<ControllerLicenseBinding>() {
+class LicenseController : LifeCycleAwareController<ControllerLicenseBinding>() {
 
   override val bindingInflater: (LayoutInflater) -> ControllerLicenseBinding = ControllerLicenseBinding::inflate
 
@@ -20,14 +25,21 @@ class LicenseController : BaseController<ControllerLicenseBinding>() {
     supportActionBar()?.title = requireContext().getString(R.string.navigation_title_license)
     supportActionBar()?.setDisplayHomeAsUpEnabled(true)
 
-    try {
-      val inputStream = requireContext().resources.openRawResource(R.raw.open_source_license)
-      val buffer = ByteArray(inputStream.available())
-      inputStream.buffered().read(buffer)
-      inputStream.close()
-      binding.webViewLicense.loadData(String(buffer), "text/html", "utf-8")
-    } catch (e: IOException) {
-      e.printStackTrace()
+    lifecycleScope.launch {
+      try {
+        val buffer: ByteArray
+        withContext(Dispatchers.IO) {
+          val inputStream = requireContext().resources.openRawResource(R.raw.open_source_license)
+          buffer = ByteArray(inputStream.available())
+          val bufferedStream = inputStream.buffered()
+          bufferedStream.read(buffer)
+          bufferedStream.close()
+          inputStream.close()
+        }
+        binding.webViewLicense.loadDataWithBaseURL(null, String(buffer), "text/html", "utf-8", null)
+      } catch (e: IOException) {
+        Timber.e(e)
+      }
     }
   }
 }
