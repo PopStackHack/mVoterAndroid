e9f06f2393bc13faad4d654e12460283c4add587
vincent_paing <dev.aungkyawpaing@gmail.com>
2020-09-21 12:25:52 +0630

    config: Up version to 4.0.0.6

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object BuildConfig {
   private const val versionMajor = 4
   private const val versionMinor = 0
   private const val versionPatch = 0
-  private const val versionBuild = 5
+  private const val versionBuild = 6
 
   const val versionName =
     "$versionMajor.$versionMinor.$versionPatch"
